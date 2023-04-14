5591983edaffd50aa40f6d279294c9db85e77232
Stefan3l <stefanel.mihaila@yahoo.com>
2023-04-14 12:37:03 +0200

    resultats

--- esercizi-stringhe-numeri/gatti-in-fila.js
+++ esercizi-stringhe-numeri/gatti-in-fila.js
@@ -1,19 +1,7 @@
-/*
-  Gatti in fila
-  Scrivi un programma che dato:
-    a) Un numero totale di gatti
-    b) Il numero dei gatti presenti in ogni fila
-  Restituisca in output:
-    - Il numero di file risultanti
-    - Nel caso l'ultima fila risulti incompleta indicare il numero di gatti mancanti per completarla (quanti gatti mancano per arrivare a b?)
+var cats = 44
+var inLine = 2
 
-  Esempio:
-    Input: numero di gatti = 44, gatti per fila = 6
-    Output: numero di file = 8, gatti mancanti = 4
+var numLines = Math.ceil(cats/inLine);
+var missing =(inLine*numLines)-cats
 
-  Consigli:
-  Per l'arrotondamento ti consiglio di non usare Math.round (arrotonda all'intero superiore solo da .5 in su),
-  utilizza Math.ceil (arrotonda sempre all'intero superiore).
-
-  http://www.imparareaprogrammare.it
-*/
+console.log ('${cats} gatti in fila per ${inLine} si unirono compatti in ${numLines} file. Nell ultima fila rimasero ${missing} posti gatto disponibili.');
